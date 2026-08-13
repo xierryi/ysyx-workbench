b5897b740f1796f59b8b97614a630e299ca3648e
231220000-Yang Xinrui <18048513952@163.com>
2026-08-13 14:51:48 +0800

    feat: refactor and remove obsolete scripts and tools
    - Delete abstract-machine-copy
    - Updated the timer implementation in the NEMU platform to correctly track uptime based on RTC readings.
    - Refactored the printf implementation in the KLIB to improve string formatting functionality.

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,21 +5,12 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int printf(const char *fmt, ...) {
-  panic("Not implemented");
-}
+static int fmt2str(char *str, const char *fmt, va_list ap) { 
 
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
-}
-
-int sprintf(char *out, const char *fmt, ...) {
-  va_list ap;
   int str_len = 0;
   int d;
   char *s;
 
-  va_start(ap, fmt);
   while (*fmt)
     switch (*fmt)
     {
@@ -31,44 +22,71 @@ int sprintf(char *out, const char *fmt, ...) {
         break;
       case 's':
         s = va_arg(ap, char *);
-        strcat(out, s);
+        strcat(str, s);
         fmt += 2;
         str_len += strlen(s);
-        out += strlen(s);
+        str += strlen(s);
         break;
       case 'd':
         d = va_arg(ap, int);
         fmt += 2;
         if(d < 0) {
-          *out ++ = '-'; 
+          *str++ = '-'; 
           d = -d;
           str_len ++;
         }
         char buf[20] = {};
-        int i;
-        for (i = 0; d; i++)
-        {
-            buf[i] = d % 10 + 48;
-            d /= 10;
-        }
+        int i = 0;
+        do {
+          buf[i] = d % 10 + 48;
+          d /= 10;
+          i ++;
+        } while(d);
         for (; i > 0; i--)
         {
-            *out ++ = buf[i - 1];
+            *str++ = buf[i - 1];
         }
         break;
       default:
         str_len ++;
-        *out ++ = *fmt ++;
+        *str++ = *fmt ++;
         break;
       }
       break;
     default:
       str_len ++;
-      *out ++ = *fmt ++;
+      *str++ = *fmt ++;
       break;
     }
-  *out = '\0';
+  *str= '\0';
+  return str_len;
+}
+
+int printf(const char *fmt, ...) {
+  int str_len = 0;
+  char buf[200]; 
+  va_list ap;
+
+  va_start(ap, fmt);
+  fmt2str(buf, fmt, ap);
   va_end(ap);
+
+  for (const char *p = buf; *p; p++) putch(*p);
+  return str_len;
+}
+
+int vsprintf(char *out, const char *fmt, va_list ap) {
+  panic("Not implemented");
+}
+
+int sprintf(char *out, const char *fmt, ...) {
+  int str_len = 0;
+  va_list ap;
+
+  va_start(ap, fmt);
+  fmt2str(out, fmt, ap);
+  va_end(ap);
+
   return str_len;
 }
 
