320cfdca8be8ae0e3707a5bcceb9bbd669a95fe4
231220000-Yang Xinrui <18048513952@163.com>
2026-08-14 17:08:38 +0800

    fix: improve rtc_io_handler to handle offset checks more clearly

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -7,10 +7,11 @@
 
 static int fmt2str(char *str, const char *fmt, va_list ap) { 
   int str_len = 0;
-  int d;
+  int d = 0;
+  int x = 0x0;
   char *s;
-  #define LENGTH_TYPE 2
-  const char fmt_type[LENGTH_TYPE] = {'s', 'd'};
+  #define LENGTH_TYPE 3
+  const char fmt_type[LENGTH_TYPE] = {'s', 'd', 'x'};
   while (*fmt)
     switch (*fmt)
     {
@@ -38,10 +39,11 @@ static int fmt2str(char *str, const char *fmt, va_list ap) {
               {
               case 's':
                 s = va_arg(ap, char *);
-                strcat(str, s);
                 fmt += placeholder_len + 1;
-                str_len += strlen(s);
-                str += strlen(s);
+                while(*s){
+                  *str ++ = *s ++;
+                  str_len ++;
+                }
                 break;
               case 'd':
                 d = va_arg(ap, int);
@@ -51,21 +53,43 @@ static int fmt2str(char *str, const char *fmt, va_list ap) {
                   d = -d;
                   str_len ++;
                 }
-                char buf[20] = {};
+                char buf_d[32] = {};
                 int i = 0;
                 do {
-                  buf[i] = d % 10 + 48;
+                  buf_d[i] = d % 10 + 48;
                   d /= 10;
                   i ++;
                 } while(d);
                 if(i < fmt_len) {
                   for(; i < fmt_len; i ++){
-                    buf[i] = place_type;
+                    buf_d[i] = place_type;
                   }
                 }
                 for (; i > 0; i--)
                 {
-                  *str++ = buf[i - 1];
+                  *str++ = buf_d[i - 1];
+                  str_len ++;
+                }
+                break;
+              case 'x':
+                x = va_arg(ap, int);
+                fmt += placeholder_len + 1;
+                char buf_x[32] = {};  
+                i = 0;
+                do {
+                  buf_x[i] = (x % 16 < 10) ? ((x % 16) + 48) : ((x % 16) + 87);
+                  x /= 16;
+                  i ++;
+                } while(x);
+                if(i < fmt_len) {
+                  for(; i < fmt_len; i ++){
+                    buf_x[i] = place_type;
+                  }
+                }
+                for (; i > 0; i--)
+                {
+                  *str++ = buf_x[i - 1];
+                  str_len ++;
                 }
                 break;
               default:
@@ -85,17 +109,75 @@ static int fmt2str(char *str, const char *fmt, va_list ap) {
   *str= '\0';
   return str_len;
 }
+// static int fmt2str(char *str, const char *fmt, va_list ap) { 
+
+//   int str_len = 0;
+//   int d;
+//   char *s;
+
+//   while (*fmt) { 
+//     switch (*fmt)
+//     {
+//     case '%'://"%" prefix match
+//       switch (*(fmt + 1))
+//       {
+//       case '\0':
+//         return str_len;
+//         break;
+//       case 's':
+//         s = va_arg(ap, char *);
+//         fmt += 2;
+//         while(*s){
+//           str_len ++;
+//           *str ++ = *s ++;
+//         }
+//         break;
+//       case 'd':
+//         d = va_arg(ap, int);
+//         fmt += 2;
+//         if(d < 0) {
+//           *str++ = '-'; 
+//           d = -d;
+//           str_len ++;
+//         }
+//         char buf[20] = {};
+//         int i = 0;
+//         do {
+//           buf[i] = d % 10 + 48;
+//           d /= 10;
+//           i ++;
+//         } while(d);
+//         for (; i > 0; i--)
+//         {
+//             *str++ = buf[i - 1];
+//         }
+//         break;
+//       default:
+//         str_len ++;
+//         *str++ = *fmt ++;
+//         break;
+//       }
+//       break;
+//     default:
+//       str_len ++;
+//       *str++ = *fmt ++;
+//       break;
+//     }
+//   }
+//   *str= '\0';
+//   return str_len;
+// }
 
 int printf(const char *fmt, ...) {
+  char out[100];
   int str_len = 0;
-  char buf[200]; 
   va_list ap;
 
   va_start(ap, fmt);
-  fmt2str(buf, fmt, ap);
+  str_len = fmt2str(out, fmt, ap);
   va_end(ap);
+  for (char *p = out; *p; p++) putch(*p);
 
-  for (const char *p = buf; *p; p++) putch(*p);
   return str_len;
 }
 
@@ -108,7 +190,7 @@ int sprintf(char *out, const char *fmt, ...) {
   va_list ap;
 
   va_start(ap, fmt);
-  fmt2str(out, fmt, ap);
+  str_len = fmt2str(out, fmt, ap);
   va_end(ap);
 
   return str_len;
